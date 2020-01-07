41868031aa6d4423c00d2df3b286f72eda3a4def
cheng.li <acheng_69@163.com>
2020-01-07 11:29:58 +0800

    1. 服务端项目初始化

---
+++ README.md
@@ -0,0 +1,1 @@
+Node.js + Vue.js 全栈项目
---
+++ server/libs/db/src/db.module.ts
@@ -0,0 +1,24 @@
+import { Module, Global } from '@nestjs/common';
+import { DbService } from './db.service';
+import { TypegooseModule } from 'nestjs-typegoose';
+import { User } from './models/user.module';
+
+// 全局定义模型
+const models = TypegooseModule.forFeature([User]);
+
+@Global()
+@Module({
+  imports: [
+    // 链接数据库
+    TypegooseModule.forRoot('mongodb://localhost/myfullstack', {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useCreateIndex: true,
+      useFindAndModify: false,
+    }),
+    models,
+  ],
+  providers: [DbService],
+  exports: [DbService, models],
+})
+export class DbModule {}
